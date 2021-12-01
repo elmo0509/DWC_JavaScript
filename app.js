d6d79dd3d67e6985dc7040e8609bad6afdb58cd4
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-12-01 23:49:37 +0900

    整数の代入 四則演算

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-alert('Hello World')
+// 変数名の前にletをつけることで、変数を宣言出来る
+// let 変数名 = 内容 ;
+let hello = 'Hello World';
+alert(hello);
+
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+
+// 四則演算
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
