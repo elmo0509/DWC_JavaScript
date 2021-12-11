fe114b5736c1215dcaed65a54a0d73afb871640e
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-12-11 23:59:17 +0900

    関数の呼び出し

--- app.js
+++ app.js
@@ -29,3 +29,16 @@ alert(2 * 6);
 
 // 割り算
 alert(10 / 2);
+
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
